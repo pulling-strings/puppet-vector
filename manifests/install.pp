7f5ad6a0a8865a70febd823c6bd05240ab18bc41
ronen <narkisr@gmail.com>
2015-08-07 01:57:46 +0300

    adding gulp build

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,18 +10,44 @@ class vector::install {
     require  => Class['nodejs']
   }
 
+  package { 'gulp':
+    provider => 'npm',
+    require  => Class['nodejs']
+  }
+
   vcsrepo { '/opt/vector':
     ensure   => present,
     provider => git,
     source   => 'git://github.com/Netflix/vector.git',
-  }
+  } -> Exec<||>
 
-  exec{'install deps':
+  exec{'bower install':
     command => 'bower install --config.interactive=false --allow-root',
     user    => 'root',
     cwd     => '/opt/vector',
     path    => ['/usr/local/node/node-default/bin/','/usr/bin/','/bin/'],
-    require => [Package['bower'], Vcsrepo['/opt/vector']]
+    require => Package['bower']
   }
 
+  file{'/usr/bin/node':
+    ensure  => link,
+    target  => '/usr/local/node/node-default/bin/node',
+    require => Class['nodejs']
+  } ->
+
+  exec{'npm install':
+    command => 'npm install',
+    user    => 'root',
+    cwd     => '/opt/vector',
+    path    => ['/usr/local/node/node-default/bin/','/usr/bin/','/bin/'],
+    require => Package['gulp']
+  }
+
+  exec{'build':
+    command => 'gulp build',
+    user    => 'root',
+    cwd     => '/opt/vector',
+    path    => ['/usr/local/node/node-default/bin/','/usr/bin/','/bin/'],
+    require => [Exec['bower install'], Exec['npm install']]
+  }
 }
